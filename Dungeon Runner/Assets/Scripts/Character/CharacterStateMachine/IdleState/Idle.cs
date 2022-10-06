efab7223a178fa4e6663f4a15b31454ba8a79fed
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-10-06 16:46:19 +0300

    Update on StateMachine

--- Dungeon Runner/Assets/Scripts/Character/CharacterStateMachine/IdleState/Idle.cs
+++ Dungeon Runner/Assets/Scripts/Character/CharacterStateMachine/IdleState/Idle.cs
@@ -6,18 +6,30 @@ public class Idle : BaseState
 {
 
     private MovementSM _stateMachine;
-    public Idle(MovementSM stateMachine) : base("Idle", stateMachine) {
-        _stateMachine = stateMachine;
-    }
 
+    private Rigidbody _rb;
+    private float _speed;
+    public Idle(MovementSM stateMachine) : base("Idle", stateMachine) {}
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     public override void Enter()
     {
         base.Enter();
+        _speed = 0;
     }
 
     public override void UpdateLogic()
     {
         base.UpdateLogic();
+        _speed = _rb.velocity.magnitude;
+        if (_speed>Mathf.Epsilon)
+        {
+            stateMachine.ChangeState(((MovementSM)stateMachine).movingState);
+        }
     }
-
+    
 }
--- Dungeon Runner/Assets/Scripts/Character/CharacterStateMachine/MovingState/Moving.cs
+++ Dungeon Runner/Assets/Scripts/Character/CharacterStateMachine/MovingState/Moving.cs
@@ -6,8 +6,29 @@ public class Moving : BaseState
 {
 
     private MovementSM _stateMachine;
-    public Moving(MovementSM stateMachine) : base("Moving", stateMachine) {
 
-        _stateMachine = stateMachine;
+    private float _speed;
+    private Rigidbody _rb;
+    public Moving(MovementSM stateMachine) : base("Moving", stateMachine) {}
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        _speed = 0;
+    }
+
+    public override void UpdateLogic()
+    {
+        base.UpdateLogic();
+        _speed = _rb.velocity.magnitude;
+        if (_speed <Mathf.Epsilon)
+        {
+            stateMachine.ChangeState(((MovementSM)stateMachine).movingState);
+        }
     }
 }
