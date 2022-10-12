fc6bc9d631a6486b4a528e6d782b1884ab67ed9c
Emre Ataş <emreatas1@hotmail.com>
2022-10-12 11:17:14 +0300

    Left background added

--- Dungeon Runner/Assets/Scripts/Level/BackgroundMovement.cs
+++ Dungeon Runner/Assets/Scripts/Level/BackgroundMovement.cs
@@ -6,8 +6,11 @@ public class BackgroundMovement : MonoBehaviour
 {
 
     public static BackgroundMovement Instance;
-    private GameObject flagObj;
+    private GameObject flagObjRight;
+    private GameObject flagObjLeft;
     private int flag = 0;
+    [SerializeField] private int _xRightTransform;
+    [SerializeField] private int _xLeftTransform;
 
     private void Awake()
     {
@@ -19,21 +22,20 @@ public class BackgroundMovement : MonoBehaviour
 
     private void Start()
     {
-        GameObject obj = _objectPool.GetPooledObject(flag);
-        obj.transform.position = new Vector3(8, 0, 24);
-        flagObj = obj;
+        GameObject objRight = _objectPool.GetPooledObject(flag);
+        objRight.transform.position = new Vector3(_xRightTransform, 0, 24);
+        flagObjRight = objRight;
+        GameObject objLeft = _objectPool.GetPooledObject(4);
+        objLeft.transform.position = new Vector3(_xLeftTransform, 0, 24);
+        flagObjLeft = objLeft;
     }
-    public void SpawnNewBackground()
+    public void SpawnRightBackground()
     {
         flag = Random.Range(0, 4);
 
         GameObject obj = _objectPool.GetPooledObject(flag);
-
-
-
-
-        obj.transform.position = new Vector3(8, 0, flagObj.transform.position.z + 15);
-        flagObj = obj;
+        obj.transform.position = new Vector3(_xRightTransform, 0, flagObjRight.transform.position.z + 15);
+        flagObjRight = obj;
 
 
 
@@ -41,6 +43,16 @@ public class BackgroundMovement : MonoBehaviour
 
     }
 
+
+    public void SpawnLeftBackground()
+    {
+        flag = Random.Range(4, 6);
+
+        GameObject obj = _objectPool.GetPooledObject(flag);
+        obj.transform.position = new Vector3(_xLeftTransform, 0, flagObjLeft.transform.position.z + 15);
+        flagObjLeft = obj;
+
+    }
 
 
 
