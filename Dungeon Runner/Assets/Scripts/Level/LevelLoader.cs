8b63401ed23c8d032c34c9638624ff7089886a0b
Emre Ataş <emreatas1@hotmail.com>
2022-10-18 17:00:42 +0300

    Test loading screen

--- Dungeon Runner/Assets/Scripts/Level/LevelLoader.cs
+++ Dungeon Runner/Assets/Scripts/Level/LevelLoader.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+
     public void LoadLevel(int sceneIndex)
     {
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
 
-        //asyncOperation.
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            Debug.Log(progress);
+
+            yield return null;
+        }
     }
 }
