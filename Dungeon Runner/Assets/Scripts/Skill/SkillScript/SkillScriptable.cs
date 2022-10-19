51038d7e18ad45db2273acac84a3b9603d4d6fa9
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-10-19 17:47:40 +0300

    Skill Data Add

---
+++ Dungeon Runner/Assets/Scripts/Skill/SkillScript/SkillScriptable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="SkillData",menuName ="SkillData/SkillData")]
+public class SkillScriptable : ScriptableObject
+{
+    
+    public enum SkillRarity
+    {
+        Common,
+        Rare,
+        Epic
+    }
+    public SkillRarity skillRarity;
+
+ public void IncreaseAttackSpeed(float attackSpeed)
+ {
+
+ }
+   
+
+
+
+
+}
