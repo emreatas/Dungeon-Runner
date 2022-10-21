305711fbae3577b2519df29aa09e9610a26359a6
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-10-21 18:45:24 +0300

    Adjustment

--- Dungeon Runner/Assets/Scripts/Character/CharacterHealth/CharacterHealth.cs
+++ Dungeon Runner/Assets/Scripts/Character/CharacterHealth/CharacterHealth.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class CharacterHealth : MonoBehaviour,IDamageable
 {
     public Transform lookAt;
+    public CharacterHealthStats characterHealthData;
+    [HideInInspector]
+    public float characterCurrentHealth;
+
+
+    private void Start()
+    {
+        characterCurrentHealth = characterHealthData.characterBaseHealth;
+    }
 
     public void TakeDamege()
     {
-        throw new System.NotImplementedException();
+        
     }
 }
