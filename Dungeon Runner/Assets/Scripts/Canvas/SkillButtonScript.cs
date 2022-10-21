bd0a56af3ec4a8c422d955008774a3b41dd277e6
Emre Ataş <emreatas1@hotmail.com>
2022-10-21 19:00:52 +0300

    Skill button added

--- Dungeon Runner/Assets/Scripts/Canvas/SkillButtonScript.cs
+++ Dungeon Runner/Assets/Scripts/Canvas/SkillButtonScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public class SkillButtonScript : MonoBehaviour
@@ -21,7 +22,13 @@ public class SkillButtonScript : MonoBehaviour
 
     [SerializeField] private int _cost;
     [SerializeField] private Skill _skill;
+    [SerializeField] private TextMeshProUGUI _costText;
 
+
+    private void Start()
+    {
+        _costText.text = "x" + _cost;
+    }
 
     public void BuySkill()
     {
@@ -32,17 +39,29 @@ public class SkillButtonScript : MonoBehaviour
             case Skill.ActivateDoubleKnife:
                 GameManager.Instance.OnActivateDoubleKnife();
                 break;
+
             case Skill.IncreaseThrowSpeed:
+                GameManager.Instance.OnIncreaseThrowSpeed();
                 break;
+
             case Skill.IncreaseAttackSpeed:
+                GameManager.Instance.OnIncreaseAttackSpeed();
                 break;
+
             case Skill.IncreaseDamage:
+                GameManager.Instance.OnIncreaseDamage();
                 break;
+
             case Skill.IncreaseRange:
+                GameManager.Instance.OnIncreaseRange();
                 break;
+
             case Skill.IncreaseReloadSpeed:
+                GameManager.Instance.OnIncreaseReloadSpeed();
                 break;
+
             case Skill.ActivateSideKnife:
+                GameManager.Instance.OnActivateSideKnife();
                 break;
         }
 
